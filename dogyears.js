4884550e23c66523670f829564d565cd468407a2
margaret <144652524+bleucrescent@users.noreply.github.com>
2023-09-12 15:42:12 -0400

    Create dogyears.js

---
+++ dogyears.js
@@ -0,0 +1,19 @@
+var myAge = 19; //my age 
+let earlyYears = 2; // early years of a dog
+earlyYears *= 10.5;
+var laterYears = myAge - 2; // later years
+
+laterYears *= 4; //dog years in later years
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//math is correct for both variables
+
+var myAgeInDogYears = earlyYears + laterYears;
+
+var myName = "Margaret".toLowerCase(); // my name in all lowercase
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
