971a1b55d90a32f9742146dd50016001b7dc5486
margaret <144652524+bleucrescent@users.noreply.github.com>
2023-09-14 21:51:24 -0400

    Update secretmessage.js

--- secretmessage.js
+++ secretmessage.js
@@ -3,7 +3,7 @@ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'ea
 secretMessage.pop(); // removes last string
 secretMessage.push('to', 'Program'); // appends to and program to the end of the array
 secretMessage[7] = 'right'; // accessing index 7 to change the element to the string right
-secretMessage.unshift('Programming'); // adds element to beginning aray
+secretMessage.unshift('Programming'); // adds element to beginning array
 secretMessage.splice(7, 4, 'know');
  //selects the index that you'd like to start removing elements from and the amount you want gone starting from the index. also adds any elements you want to replace from that index too.
 
